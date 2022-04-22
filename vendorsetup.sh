d4be005835f310c2a4fb4eec683f9774cd20959f
Anoosragh <83896147+ok-ano0s@users.noreply.github.com>
2022-04-22 21:52:00 +0530

    RMX1941: Update framework/base link

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,5 +11,5 @@ git clone -b twelve-L https://github.com/CipherOS/android_packages_modules_Wifi 
 git clone -b twelve-L https://github.com/CipherOS/android_frameworks_opt_telephony frameworks/opt/telephony
 git clone -b twelve-L https://github.com/CipherOS/android_frameworks_opt_net_ims frameworks/opt/net/ims
 git clone -b lineage-19.1 https://github.com/realme-mt6785-devs/android_system_security system/security
-git clone -b 12.1 https://github.com/Forks69/platform_frameworks_base frameworks/base
+git clone -b 12.1 https://github.com/Palladium-tmp/platform_frameworks_base-mtk frameworks/base
 git clone -b lineage-19.0 https://github.com/Realme-G70-Series/android_packages_apps_RealmeDirac packages/apps/RealmeDirac
